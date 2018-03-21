c19a7383211ed1b303cd41cb06a471ee47243dc5
keepitsimle <easy_bboy@126.com>
2018-03-22 00:06:56 +0800

    add
    add tree

--- 机器学习司长/bugs.py
+++ 机器学习司长/bugs.py
@@ -8,22 +8,20 @@ from math import log
 #         [2,3,'no'],
 #     ]
 
-# def cal_shonnan_entropy(dataSet):
-#     lens = len(dataSet)
-#     s = {}
-#     for items in dataSet:
-#         feature = items[-1]
-#         print("feature",feature);
-#         if feature not in s.keys():
-#             s[feature] = 0
-#         s[feature]+=1
-#     print(s)
-#
-#     entropy = 0
-#     for items in s:
-#         pro = s[items]/lens
-#         entropy += pro*log(pro,2)
-#     return entropy
+def cal_shonnan_entropy(dataSet):
+    lens = len(dataSet)
+    s = {}
+    for items in dataSet:
+        feature = items[-1]
+        print("feature",feature);
+        if feature not in s.keys():
+            s[feature] = 0
+        s[feature]+=1
+    entropy = 0
+    for items in s:
+        pro = s[items]/lens
+        entropy += pro*log(pro,2)
+    return entropy
 
 # def listToStr(listt):
 #     strf = ''
@@ -73,13 +71,13 @@ from math import log
     输入: 训练集D = {(x1,y1),(x2,y2),...(xm,ym)}
             属性集 A = {a1,a2,a3,,,ad}
     1.生产一个节点node
-    2. if D 中的样本属于同一类别C 
+    2. if D 中的样本属于同一类别C
          将node标记为C类叶子节点; return
        end if
-    3  if A = 空 或者 D中样本在A的取值都一样 
+    3  if A = 空 或者 D中样本在A的取值都一样
           将node 标记为叶子节点,类别是D中样本最多的类,
     4  从A中选择最佳的划分a
-         
+
 '''
 '''
     返回label最多的class
@@ -110,6 +108,38 @@ def majority_choose(class_list):
             return items
         return ''
 
+def split_data_set(dataSet,index,value):
+    ret_data_set = {}
+    for item in dataSet:
+        if item[index] == value:
+            reduce_feature = dataSet[:index]
+            reduce_feature.extend(dataSet[index+1:-1])
+        ret_data_set.append(reduce_feature)
+    return ret_data_set
+
+
+
+
+def choose_best_feature_to_split(dataSet):
+    number_features = len(dataSet[0]) - 1 #最后的一个是label
+    base_entropy = cal_shonnan_entropy(dataSet)
+    max_info_diff = 0.0
+    best_index = -1
+    for i in range(number_features):
+        feature_list = [example[i] for example in dataSet] # 取到每一个feature
+        unique = set(feature_list)
+        for unquireval in unique:
+            sub_class = split_data_set(dataSet,i,unquireval)
+            cal_info_diff = base_entropy - cal_shonnan_entropy(sub_class)
+            if cal_info_diff > max_info_diff:
+                max_info_diff = cal_info_diff
+                best_index = i
+    return best_index
+
+
+
+
+
 
 def create_tree(dataSet,labels):
     class_list = [ex[-1] for ex in dataSet] #list_comprehension
@@ -117,7 +147,17 @@ def create_tree(dataSet,labels):
         return class_list[0]
     if 1 == len(class_list):
         return majority_choose(class_list) #返回样本中最多的class
-    # best_choose = 
+    best_choose = choose_best_feature_to_split(dataSet)
+    best_feature_label = labels[best_choose]
+    my_tree = {best_feature_label:{}}
+    del(labels[best_choose])
+    featValues = [ex[best_choose] for ex in dataSet]
+    uniqueVals = set(featValues)
+    for value in uniqueVals:
+        subLabels = labels[:]
+        my_tree[best_feature_label][value] = create_tree(split_data_set(
+            dataSet,best_choose,value),subLabels)
+    return my_tree
 
 
 #
@@ -155,3 +195,4 @@ dataSet1 =  [
 ];
 
 print(majority_choose(dataSet1))
+
